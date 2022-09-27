426bd9dd8d1b5271bba5a2d7276ed2407023c728
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 <lpenrose@depaul.edu>
2022-09-26 19:49:27 -0500

    updated function update user

---
+++ Javascript/updateUser.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('formElem');
+
+form.addEventListener('submit',function (e){
+    e.preventDefault();
+    const prepayload = new FormData(form)
+
+    const payload = new URLSearchParams(prepayload);
+
+    let fname = payload.get("firstName");
+    let lname = payload.get("lastName");
+    let eml = payload.get("email");
+    let pswd = payload.get("password");
+    let role = payload.get("role");
+
+    console.log(eml)
+    console.log(pswd)
+    let data = {
+        fistName:fname,
+        lastName:lname,
+        email:eml,
+        password: pswd,
+        role: role
+    }
+
+    fetch('http://localhost:8080/api/v1/updateuserinfo', {
+        method: "POST",
+        headers: {
+            "Content-Type":'application/json'
+        },
+        body: JSON.stringify(data),
+
+    }).then(r=>(alert("Updated user: " + eml)))
+
+})
