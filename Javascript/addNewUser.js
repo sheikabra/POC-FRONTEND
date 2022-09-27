0c3b9011b1f6420454a3b75e8addcebea9032e42
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 <lpenrose@depaul.edu>
2022-09-27 01:04:41 -0500

    Created add new User function, updated styles

---
+++ Javascript/addNewUser.js
@@ -0,0 +1,34 @@
+
+    const form = document.getElementById('formElem');
+
+form.addEventListener('submit',function (e){
+    e.preventDefault();
+    const prepayload = new FormData(form)
+
+    const payload = new URLSearchParams(prepayload);
+
+    let fname = payload.get("firstName");
+    let lname = payload.get("lastName");
+    let eml = payload.get("email");
+    let pswd = payload.get("password");
+
+    console.log(eml)
+    console.log(pswd)
+
+    let data = {
+        fistName:fname,
+        lastName:lname,
+        email:eml,
+        password: pswd
+    }
+
+    fetch('http://localhost:8080/api/v1/registration', {
+        method: "POST",
+        headers: {
+            "Content-Type":'application/json'
+        },
+        body: JSON.stringify(data),
+
+    }).then(r=>(alert("Added New User: " + eml)))
+
+})
